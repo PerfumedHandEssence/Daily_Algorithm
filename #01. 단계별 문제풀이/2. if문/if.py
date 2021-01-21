bd8933a86eede4fcf292ff6023b732167c16c74a
PerfumedHandEssence <67088292+PerfumedHandEssence@users.noreply.github.com>
2021-01-21 13:35:23 +0900

    .
    .

---
+++ #01. 단계별 문제풀이/2. if문/if.py
@@ -0,0 +1,63 @@
+# A = int(input())
+# B = int(input())
+# print(A*(B%10))
+# print(A*(B//10))
+# print(A*(B//100))
+# print(A*B)
+
+# A, B = map(int, input().split())
+# if A < B :
+#     print("<")
+# elif A > B :
+#     print(">")
+# else:
+#     print("=")
+
+# A = int(input())
+#
+# if A >= 90:
+#     print("A")
+# elif A>=80:
+#     print("B")
+# elif A>=70:
+#     print("C")
+# elif A>=60:
+#     print("D")
+# else:
+#     print("F")
+
+# A = int(input())
+# if (A%100)==0:
+#     if(A%400)==0:
+#         print("1")
+#     else:
+#         print("0")
+# elif (A%4)==0:
+#     print("1")
+# else :
+#     print("0")
+
+# A = int(input())
+# B = int(input())
+# if A>0 :
+#     if B>0:
+#         print("1")
+#     else :
+#         print("4")
+# else :
+#     if B>0:
+#         print("2")
+#     else :
+#         print("3")
+# 
+# A, B = map(int, input().split())
+# if B<45 :
+#     B = B+15
+#     if A == 0:
+#         A += 23
+#     else :
+#         A = A-1
+# else:
+#     B = B-45
+# 
+# print(A,B)
